00d829f259e584a60d95df5fe72d03b1ec4fe699
李子园 <73676543+venom-lemon@users.noreply.github.com>
2022-09-03 16:54:06 +0800

    初始化项目

---
+++ liblog-gateway/src/main/java/com/liblog/gateway/config/LiblogCorsConfiguration.java
@@ -0,0 +1,30 @@
+package com.liblog.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @Author: liZiYuan
+ * <p>
+ * Date: 2022/9/3  21:19
+ */
+
+@Configuration
+public class LiblogCorsConfiguration {//网关统一处理跨域
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
